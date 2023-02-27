1b30f1dce5760418c3a26041386fb671c5c9907c
owalid <othmane.el.ayadi@gmail.com>
2023-02-27 18:07:31 +0100

    refacto: clean code

--- utils.py
+++ utils.py
@@ -10,6 +10,6 @@ def parse_command(input):
     arguments = input_split[1:]
 
     if input not in ['status', 'restart', 'stop', 'start']:
-        return False, False
+        return (False, False)
     
     return (input, arguments)
