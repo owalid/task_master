82abb0ca5f78671a7fd041aa500bfd7f01b37c9f
owalid <othmane.el.ayadi@gmail.com>
2023-02-27 20:14:45 +0100

    feat: init server class (#30)
    * feat: init server class
    
    * refacto: clean code

--- client.py
+++ client.py
@@ -5,11 +5,15 @@ from server import SOCK_FILE
 class Client:
     def __init__(self):
         self.client_socket = None
-        if not os.path.exists(SOCK_FILE):
-            print("unix:///tmp/server.sock no such file")
-        else:
-            self.client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            self.connect()
+        try:
+            if not os.path.exists(SOCK_FILE):
+                print(f"unix://{SOCK_FILE} no such file")
+            else:
+                self.client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+                self.connect()
+        except ConnectionRefusedError:
+            print(f"Connection refused: unix://{SOCK_FILE}")
+            exit(1)
 
     def connect(self):
         self.client_socket.connect(SOCK_FILE)
