07ace2aa699bd03cf9fc8c59f503e32623a00051
kibatche <kibatche@kibatche.com>
2023-04-04 17:55:45 +0200

    correction d'un bug pour autostart + ajout d'un fichier de config avec 1001 jobs

--- gen.py
+++
@@ -1,6 +0,0 @@
-for i in range(0,1000):
-    print(f"  php{i}:")
-    print(f'    cmd: "/usr/bin/php -S 0:'+ str(4242 + i) + '"')
-    print('    numprocs: 1')
-    print('    umask: 022')
-    print('    workingdir: /tmp')
